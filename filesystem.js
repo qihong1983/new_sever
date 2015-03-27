5b3d59349de2ba1d3c1a60d8061eebd7fa0f415e
qihong <qihongbin@163.com>
2015-03-28 05:27:05 +0800

    asdfasdf

--- filesystem.js
+++ filesystem.js
@@ -7,12 +7,17 @@
  */
 
 
-var fs=require("fs"),
-    http=require("http"),
-    url=require("url"),
-    path=require("path"),
-    mime=require("./mime").mime,
-    util=require('util');
+var fs = require("fs"),
+
+    http = require("http"),
+
+    url = require("url"),
+
+    path = require("path"),
+
+    mime = require("./mime").mime,
+
+    util = require('util');
 
 /**
  * www根目录
@@ -20,38 +25,53 @@ var fs=require("fs"),
 
 var root=__dirname;
 
-console.log(root,'rootrootrootroot');
 /** 
  * 配置abc.json
  */
 
 var abc=JSON.parse(fs.readFileSync('./abc.json','utf-8'));
+
 var mock_online_address = abc.mock_online_address;
+
 var online_host = abc.online_host;
+
 var online_port = abc.online_port;
+
 var local_host = abc.local_host;
+
 var local_port = abc.local_port;
     
-if(!path.existsSync(root)){
+if (!path.existsSync(root)) {
+
     util.error(root+"文件夹不存在，请重新制定根文件夹！");
+
     process.exit();
+
 }
     
 /**
  * 显示文件夹下面的文件
  */
-function listDirectory(parentDirectory,req,res){
-    fs.readdir(parentDirectory,function(error,files){
-        var body=formatBody(parentDirectory,files);
+function listDirectory(parentDirectory, req,res) {
 
-        res.writeHead(200,{
-            "Content-Type":"text/html;charset=utf-8",
-            "Content-Length":Buffer.byteLength(body,'utf8'),
-            "Server":"NodeJs("+process.version+")"
+    fs.readdir(parentDirectory, function(error, files) {
+
+        var body = formatBody(parentDirectory, files);
+
+        res.writeHead(200, {
+
+            "Content-Type" : "text/html;charset=utf-8",
+
+            "Content-Length" : Buffer.byteLength(body, 'utf8'),
+
+            "Server":"NodeJs(" + process.version + ")"
+
         });
 
-        res.write(body,'utf8');
+        res.write(body, 'utf8');
+
         res.end();
+
     });
 
 }
@@ -59,66 +79,106 @@ function listDirectory(parentDirectory,req,res){
 /**
  * 显示文件内容
  */
-function showFile(file,req,res){
-	var hashStr = req.url;
-	var hash = require("crypto").createHash('sha1').update(hashStr).digest('base64');
-    fs.readFile(filename,'binary',function(err,file){
-        var contentType=mime.lookupExtension(path.extname(filename));
+function showFile(file, req, res) {
 
-        if(req.headers['if-none-match'] == hash){
+	var hashStr = req.url;
+
+	var hash = require("crypto").createHash('sha1').update(hashStr).digest('base64');
+
+    fs.readFile(filename, 'binary', function(err, file) {
+
+        var contentType = mime.lookupExtension(path.extname(filename));
+
+        if(req.headers['if-none-match'] == hash) {
+
 			res.writeHead(304);
+
 			res.end();
+
 			return;
+
 		}
 
-        res.writeHead(200,{
-            "Content-Type":contentType,
-            "Content-Length":Buffer.byteLength(file,'binary'),
-            "Server":"NodeJs("+process.version+")",
+        res.writeHead(200, {
+
+            "Content-Type" : contentType,
+
+            "Content-Length" : Buffer.byteLength(file, 'binary'),
+
+            "Server" : "NodeJs(" + process.version + ")",
+
             "Etag": hash
+
         });
+
         res.write(file,"binary");
+
         res.end();
-    })
+
+    });
 }
 
 /**
  * 在Web页面上显示文件列表，格式为<ul><li></li><li></li></ul>
  */
-function formatBody(parent,files){
+function formatBody(parent, files) {
 
-    var res=[],
-        length=files.length;
+    var res = [],
+
+        length = files.length;
 
     res.push("<!doctype>");
+
     res.push("<html>");
+
     res.push("<head>");
-    res.push("<meta http-equiv='Content-Type' content='text/html;charset=utf-8'></meta>")
+
+    res.push("<meta http-equiv='Content-Type' content='text/html;charset=utf-8'></meta>");
+
     res.push("<title>Node.js文件服务器</title>");
+
     res.push("</head>");
+
     res.push("<body width='100%'>");
+
     res.push("<ul>");
-    files.forEach(function(val,index){
-        var stat=fs.statSync(path.join(parent,val));
-        if(stat.isDirectory(val)){
-            val=path.basename(val)+"/";
-        }else{
-            val=path.basename(val);
+
+    files.forEach(function (val, index) {
+
+        var stat = fs.statSync(path.join(parent,val));
+
+        if (stat.isDirectory(val)) {
+
+            val = path.basename(val) + "/";
+
+        } else {
+
+            val = path.basename(val);
+
         }
+
         res.push("<li><a href='"+val+"'>"+val+"</a></li>");
+
     });
+
     res.push("</ul>");
+
     res.push("<div style='position:relative;width:98%;bottom:5px;height:25px;background:yellow;'>");
+
     res.push("<div style='margin:0 auto;height:100%;line-height:25px;text-align:center'>Powered By Node.js</div>");
-    res.push("</div>")
+
+    res.push("</div>");
+
     res.push("</body>");
+
     return res.join("");
+
 }
 
 /**
  * 如果文件找不到，显示404错误
  */
-function write404(req, res){
+function write404(req, res) {
 
     var body="文件不存在:-(";
 
@@ -128,7 +188,7 @@ function write404(req, res){
 
         "Content-Length":Buffer.byteLength(body,'utf8'),
 
-        "Server":"NodeJs("+process.version+")"
+        "Server":"NodeJs(" + process.version + ")"
 
     });
 
@@ -142,13 +202,7 @@ function write404(req, res){
  * 判断文件是否存在,路径问题是否带有“/”
  */
  var exists_i = 0;
-function existsFile(filename, req, res, pathname){
-			//console.log(filename , 'asdfasd_____________-');
-			// console.log(filename.split('\\'),'asdfasfasdfsadf_+_+_+_+_+_+_');
-			// console.log(pathname);
-
-	  //       exists_i++;
-	  //       console.log(exists_i);
+function existsFile(filename, req, res, pathname) {
 
 	    path.exists(filename, function(exists) {
 
@@ -168,53 +222,97 @@ function existsFile(filename, req, res, pathname){
 
                     } else if (stat.isDirectory()){
 
-		                if (filename.charAt(filename.length -1) != '\\') {
 
-							res.writeHead(302, {
+							listDirectory(filename, req,res);
+		     //            if (filename.charAt(filename.length -1) != '\\') {
 
-						        "Location":pathname+'/'
+							// res.writeHead(302, {
 
-						    });
+						 //       // "Location":pathname+'/'
 
-							res.end();
-							 //return ;
-                    	}
+							// 	 "Location": pathname + '/'
+						    
+						 //    });
 
-                        listDirectory(filename,req,res);
+							// res.end();
+							//  //return ;
+       //              	}
+
+                        //listDirectory(filename,req,res);
                     }
                 });
             }
         });
 } 
 
+function getFileName(str){
+
+	var reg = /[^\\\/]*[\\\/]+/g;
+	
+	//xxx\或者是xxx/
+
+	str = str.replace(reg,'');
+
+	return str;
+
+}
+
 /**
  * 如果是combo
  */
 function comboPress (pathname, req,res) {
+
 
 	var hashStr = req.url;
 
 	var hash = require("crypto").createHash('sha1').update(hashStr).digest('base64');
 
 	var filename = [];
-
+	var filename_source = [];
+	var filename_t = [];
 	for (var i = 0; i < pathname.length; i++) {
 
-		var temp = path.join(root, pathname[i]);
+		filename_source[i] = pathname[i];
 
-		filename.push(temp);
+		pathname[i] = filename_source[i].split(mock_online_address)[1];
+		 
+
+		console.log(filename_source[i],'adsfasdf');
+
+		
+
+
+		console.log(pathname[i],i);
+
+		if (pathname[i] == undefined) {
+
+			//取线上
+			pathname[i] = online_host+ ':'+online_port+filename_source[i];
+			
+			//var temp = path.join(root, pathname[i]);
+
+			filename.push(pathname[i]);
+		   // return ;
+		} else {
+			var temp = path.join(root, pathname[i]);
+
+			filename.push(temp);
+		}
+
+		
 
 	}
 
 	var temp_file = '';
 
-	function seqRequest(i,limit){
-
+	function seqRequest(i, limit) {
+		console.log(filename[i],'------------------------');
+		 filename_t[i] = filename[i];
 		fs.exists(filename[i], function( exists ) {
 
 			if (exists) {
 
-				fs.readFile(filename[i],'utf-8', function (err, file) {
+				fs.readFile(filename[i], 'utf-8', function (err, file) {
 
 					if (err == null) {
 						
@@ -255,20 +353,140 @@ function comboPress (pathname, req,res) {
 					        res.end();
 
 						}
+					} else {
+						if (err.code == 'EISDIR') {
+							 var body="400 Bad Request";
+							res.writeHead(400, {
+
+					        "Content-Type":"text/html;charset=utf-8",
+
+					        "Content-Length":Buffer.byteLength(body,'utf8'),
+
+					        "Server":"NodeJs(" + process.version + ")"
+
+					    });
+
+					    res.write(body);
+
+					    res.end();
+						}
+
+
 					}
 				});
+			} else {
+
+				// console.log(filename_source[i],'&&&&&&&&');
+				// console.log(temp_file);
+				// console.log(online_host+':'+online_port+filename_source[i]);
+				var reqData='';
+				var post_options = {
+				    host: online_host,
+				    port: online_port,
+				    path: filename_source[i],
+				    method: 'GET',
+				    headers: {
+				      'Content-Type': "text/plain;charset=utf-8",
+				      'Content-Length': reqData.length
+				    }
+				  };
+					//http.get('http://'+online_host+':'+online_port+filename_source[i], function (res) {
+					http.get('http://' + online_host + ':' + filename_source[i], function (response) {
+
+						console.log(response.statusCode);
+
+						if (response.statusCode == 404) {
+							var body="文件不存在:-(";
+
+						    res.writeHead(404, {
+
+						        "Content-Type":"text/html;charset=utf-8",
+
+						        "Content-Length":Buffer.byteLength(body,'utf8'),
+
+						        "Server":"NodeJs(" + process.version + ")"
+
+						    });
+
+						    res.write(body);
+
+						    res.end();
+
+						    return ;
+						}
+
+
+
+				         
+				 
+				         response.on("data",function (chunk) {
+				 
+				             temp_file += chunk;
+				 
+				         }).on("end", function () {
+				 
+				            res.writeHead(200, {
+
+						        "Content-Type":"text/plain;charset=utf-8",
+
+						        "Content-Length":Buffer.byteLength(temp_file,'utf8'),
+
+						        "Server":"NodeJs(" + process.version + ")"
+
+						    });
+
+						    res.write(temp_file);
+
+						    res.end();
+				 
+				         }).on('error', function (e) {
+				 
+				             console.log("Got error: " + e.message);
+				 
+				         });
+
+
+					});
+
+
+
+   
+   
+
+				//url combo的文件没有找到怎么办
+
+				// console.log(filename);
+				// console.log(pathname);
+
+				    // var body="文件不存在:-(";
+
+				    // res.writeHead(404, {
+
+				    //     "Content-Type":"text/html;charset=utf-8",
+
+				    //     "Content-Length":Buffer.byteLength(body,'utf8'),
+
+				    //     "Server":"NodeJs(" + process.version + ")"
+
+				    // });
+
+				    // res.write(body);
+
+				    // res.end();
 			}
+
 		});
 	   
 	}
 
-	seqRequest(0,filename.length-1);
+	seqRequest(0, filename.length - 1);
+	
 }
 
 /**
  * 创建服务器
  */
-http.createServer(function(req,res){
+http.createServer(function (req, res) {
 
     //将url地址的中的%20替换为空格，不然Node.js找不到文件
 
@@ -281,65 +499,113 @@ http.createServer(function(req,res){
 	var hash = require("crypto").createHash('sha1').update(hashStr).digest('base64');
 
     //能够正确显示中文，将三字节的字符转换为utf-8编码
-    pathname=pathname.replace(re,function(word){
+    pathname=pathname.replace(re, function (word) {
 
-        var buffer=new Buffer(3),
+        var buffer = new Buffer(3),
 
-            array=word.split('%');
+            array = word.split('%');
 
         array.splice(0, 1);
 
-        array.forEach(function(val, index){
+        array.forEach(function (val, index) {
 
             buffer[index]=parseInt('0x'+val, 16);
 
         });
 
         return buffer.toString('utf8');
+
     });
 
-	//pathname = '/';
-    //pathname = url.resolve(pathname,mock_online_address);
-
-    //pathname = ['/t1.js','/t2.js','/t3.js'];
-
-	function getFileName(str){
-
-		var reg = /[^\\\/]*[\\\/]+/g;
-
-		//xxx\或者是xxx/
-
-		str = str.replace(reg,'');
-
-		return str;
-
-	}
-
-
-	console.log(getFileName(pathname),'asdfasdfasdfasdfasdf');
 
 	pathname = getFileName(pathname);
 
-	if (url.parse(req.url).pathname.split(pathname)[0] === mock_online_address) {
 
-		pathname = '/' + pathname;
+	if (url.parse(req.url).pathname.split(mock_online_address)[1] == undefined) {
 
-	} else if (url.parse(req.url).pathname.split(pathname)[0] === '/') {
+		if (url.parse(req.url).pathname.split(pathname)[0] === mock_online_address) {
 
-		pathname = '/';
+			pathname = url.parse(req.url).pathname.split(mock_online_address)[0] + pathname;
+			
+		} else if (url.parse(req.url).pathname.split(pathname)[0] === '/') {
 
+
+			var mock_online_address_temp = mock_online_address.substr(1,mock_online_address.length);
+
+			if (url.parse(req.url).path.indexOf(mock_online_address_temp) > 0) {
+
+				pathname = url.parse(req.url).pathname.split(mock_online_address)[0];
+
+			} else {
+
+				// var body="文件不存在:-(";
+
+			 //    res.writeHead(404, {
+
+			 //        "Content-Type":"text/html;charset=utf-8",
+
+			 //        "Content-Length":Buffer.byteLength(body,'utf8'),
+
+			 //        "Server":"NodeJs(" + process.version + ")"
+
+			 //    });
+
+			 //    res.write(body);
+
+			 //    res.end();
+			}
+
+		} else {
+
+			var mock_online_address_temp = mock_online_address.substr(1,mock_online_address.length);
+
+			if (url.parse(req.url).path.indexOf(mock_online_address_temp) > 0) {
+
+				pathname = url.parse(req.url).pathname.split(mock_online_address)[0];
+
+			} else {
+				var body="文件不存在:-(";
+
+			    res.writeHead(404, {
+
+			        "Content-Type":"text/html;charset=utf-8",
+
+			        "Content-Length":Buffer.byteLength(body,'utf8'),
+
+			        "Server":"NodeJs(" + process.version + ")"
+
+			    });
+
+			    res.write(body);
+
+			    res.end();
+			}
+
+		}
 	} else {
 
-		console.log('other');
+		if (url.parse(req.url).pathname.split(pathname)[0] === mock_online_address) {
 
+			pathname = '/' + pathname;
+
+		} else if (url.parse(req.url).pathname.split(pathname)[0] === '/') {
+
+
+			pathname = url.parse(req.url).pathname.split(mock_online_address)[1];
+
+		} else {
+			//console.log(url.parse(req.url).pathname.split(mock_online_address)[1]);
+			//把文件和路径切出来
+
+			pathname = url.parse(req.url).pathname.split(mock_online_address)[1];
+
+		}
 	}
 
 
 	if (req.url.indexOf("??") == 1) {
 
 		var arr = req.url.split("??")[1].split(',');
-
-		console.log(arr);
 
 		var files = [];
 
@@ -349,18 +615,19 @@ http.createServer(function(req,res){
 
 		});
 
+		//console.log(files,'files');
+
 		pathname = files;
 
 		for (var i = 0; i < pathname.length; i++) {
 
-			//console.log(pathname[i].split("?"),'pathname[i].split("?")');
 			pathname[i] = pathname[i].split("?")[0];
 
 		}
 	}
 
-	console.log(pathname);
-	
+
+
     if (pathname=='/') {
 
         listDirectory(root,req,res);
@@ -369,7 +636,7 @@ http.createServer(function(req,res){
 
     	//如果等于1,url就是有??
     	if (req.url.indexOf("??") == 1) {
-
+    		 console.log(pathname,'????????????');
     		comboPress(pathname, req, res);
 
     	} else {
@@ -379,6 +646,7 @@ http.createServer(function(req,res){
         	existsFile(filename,req,res,pathname);
 
     	}
+
     }
 
 }).listen(local_port, local_host).on('close',function (stat) {
