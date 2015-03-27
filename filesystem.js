be08936d93741b6fbeb31e45d6dda3c851dc8e8e
qihong <qihongbin@163.com>
2015-03-27 15:22:42 +0800

    lll

--- filesystem.js
+++ filesystem.js
@@ -1,8 +1,11 @@
 /***
- * @author flyingzl
- * @date 2010-11-27
- * 一个基于Node.js的简单文件服务器
+ * @author 齐洪
+ * @date 2015-03-26
+ * 一个基于Node.js的简单文件服务器并写入url combo功能
+ *     combo 例：
+ *			 http://127.0.0.1/??t1.js,t2.js?t=333332323,t3.js
  */
+
 
 var fs=require("fs"),
     http=require("http"),
@@ -11,41 +14,53 @@ var fs=require("fs"),
     mime=require("./mime").mime,
     util=require('util');
 
-//www根目录
-var root=__dirname,
-    host="127.0.0.1",
-    port="8888";
+/**
+ * www根目录
+ */
 
+var root=__dirname;
 
+/** 
+ * 配置abc.json
+ */
 
+var abc=JSON.parse(fs.readFileSync('./abc.json','utf-8'));
+var mock_online_address = abc.mock_online_address;
+var online_host = abc.online_host;
+var online_port = abc.online_port;
+var local_host = abc.local_host;
+var local_port = abc.local_port;
     
 if(!path.existsSync(root)){
     util.error(root+"文件夹不存在，请重新制定根文件夹！");
     process.exit();
 }
     
-//显示文件夹下面的文件
+/**
+ * 显示文件夹下面的文件
+ */
 function listDirectory(parentDirectory,req,res){
     fs.readdir(parentDirectory,function(error,files){
         var body=formatBody(parentDirectory,files);
-
-
 
         res.writeHead(200,{
             "Content-Type":"text/html;charset=utf-8",
             "Content-Length":Buffer.byteLength(body,'utf8'),
             "Server":"NodeJs("+process.version+")"
         });
+
         res.write(body,'utf8');
         res.end();
     });
 
 }
 
-//显示文件内容
+/**
+ * 显示文件内容
+ */
 function showFile(file,req,res){
-var hashStr = req.url;
-var hash = require("crypto").createHash('sha1').update(hashStr).digest('base64');
+	var hashStr = req.url;
+	var hash = require("crypto").createHash('sha1').update(hashStr).digest('base64');
     fs.readFile(filename,'binary',function(err,file){
         var contentType=mime.lookupExtension(path.extname(filename));
 
@@ -66,13 +81,14 @@ var hash = require("crypto").createHash('sha1').update(hashStr).digest('base64')
     })
 }
 
-//在Web页面上显示文件列表，格式为<ul><li></li><li></li></ul>
+/**
+ * 在Web页面上显示文件列表，格式为<ul><li></li><li></li></ul>
+ */
 function formatBody(parent,files){
-
-
 
     var res=[],
         length=files.length;
+
     res.push("<!doctype>");
     res.push("<html>");
     res.push("<head>");
@@ -80,7 +96,7 @@ function formatBody(parent,files){
     res.push("<title>Node.js文件服务器</title>");
     res.push("</head>");
     res.push("<body width='100%'>");
-    res.push("<ul>")
+    res.push("<ul>");
     files.forEach(function(val,index){
         var stat=fs.statSync(path.join(parent,val));
         if(stat.isDirectory(val)){
@@ -91,14 +107,16 @@ function formatBody(parent,files){
         res.push("<li><a href='"+val+"'>"+val+"</a></li>");
     });
     res.push("</ul>");
-    res.push("<div style='position:relative;width:98%;bottom:5px;height:25px;background:gray'>");
+    res.push("<div style='position:relative;width:98%;bottom:5px;height:25px;background:yellow;'>");
     res.push("<div style='margin:0 auto;height:100%;line-height:25px;text-align:center'>Powered By Node.js</div>");
     res.push("</div>")
     res.push("</body>");
     return res.join("");
 }
 
-//如果文件找不到，显示404错误
+/**
+ * 如果文件找不到，显示404错误
+ */
 function write404(req,res){
     var body="文件不存在:-(";
     res.writeHead(404,{
@@ -109,6 +127,9 @@ function write404(req,res){
     res.write(body);
     res.end();
 }
+/**
+ * 判断文件是否存在,路径问题是否带有“/”
+ */
 function existsFile(filename,req,res){
 	        path.exists(filename,function(exists){
             if(!exists){
@@ -137,7 +158,9 @@ function existsFile(filename,req,res){
         });
 } 
 
-//如果是combo
+/**
+ * 如果是combo
+ */
 function comboPress (pathname, req,res) {
 	var hashStr = req.url;
 	var hash = require("crypto").createHash('sha1').update(hashStr).digest('base64');
@@ -184,7 +207,10 @@ function comboPress (pathname, req,res) {
 
 	seqRequest(0,filename.length-1);
 }
-//创建服务器
+
+/**
+ * 创建服务器
+ */
 http.createServer(function(req,res){
     //将url地址的中的%20替换为空格，不然Node.js找不到文件
     var pathname=url.parse(req.url).pathname.replace(/%20/g,' '),
@@ -204,8 +230,6 @@ http.createServer(function(req,res){
     });
 
     //pathname = ['/t1.js','/t2.js','/t3.js'];
-
-
 
     if (req.url.indexOf("??") == 1) {
 		var arr = req.url.split("??")[1].split(',');
@@ -236,8 +260,12 @@ http.createServer(function(req,res){
         	existsFile(filename,req,res);
     	}
     }
-}).listen(port,host).on('close',function (stat) {
+
+}).listen(local_port,local_host).on('close',function (stat) {
 	console.log(stat);
 });
 
-util.debug("服务器开始运行 http://"+host+":"+port)
+/**
+ * 服务器开启提示
+ */
+util.debug("服务器开始运行 http://"+local_host+":"+local_port);
